115e063087abac02012c2b599d02670412db37b1
majeedabudbay <48244398+majeedabudbay@users.noreply.github.com>
2020-05-16 19:27:42 +0300

    finished

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const path = require('path')
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/weatherDB', { useNewUrlParser: true ,useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/weatherDB', { useNewUrlParser: true ,useUnifiedTopology: true})
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', api)
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, function () {
     console.log(`Running on port ${port}`)
 })
